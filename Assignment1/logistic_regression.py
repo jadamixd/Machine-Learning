65f2cf56757016f49f4a37db59177e0c6b6c9784
Jakob Gundersen <gmboost13@gmail.com>
2024-09-03 23:43:09 +0200

    Finished task 2, i think...

--- Assignment1/logistic_regression.py
+++ Assignment1/logistic_regression.py
@@ -1,17 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.preprocessing import PolynomialFeatures, MinMaxScaler
 
 class LogisticRegression:
-    def __init__(self, learning_rate=0.1, epochs=10000, degree=100):
+    def __init__(self, learning_rate=0.1, epochs=1000, degree=10, threshold=0.5):
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.degree = degree
         self.weights = None
         self.bias = None
-        self.losses = []  
+        self.losses = []
+        self.accuracies = []  
         self.poly = PolynomialFeatures(degree=self.degree, include_bias=False)
-        self.scaler = StandardScaler()
+        self.scaler = MinMaxScaler()
+        self.threshold = threshold
 
     def fit(self, X, y):
         X_poly = self.poly.fit_transform(X)
@@ -25,8 +27,12 @@ class LogisticRegression:
             lin_model = np.dot(X_poly_scaled, self.weights) + self.bias
             y_pred = 1 / (1 + np.exp(-lin_model))
 
-            loss = -np.mean(y * np.log(y_pred + 1e-15) + (1 - y) * np.log(1 - y_pred + 1e-15)) #
+            loss = -np.mean(y * np.log(y_pred + 1e-15) + (1 - y) * np.log(1 - y_pred + 1e-15)) #1e-15 is added in case y_pred is one to prevent errors :)
             self.losses.append(loss)
+
+            y_pred_bin = np.where(y_pred > self.threshold, 1, 0)
+            accuracy = np.mean(y_pred_bin == y)
+            self.accuracies.append(accuracy)
 
             dw = (1 / m) * np.dot(X_poly_scaled.T, (y_pred - y))
             db = (1 / m) * np.sum(y_pred - y)
@@ -40,5 +46,5 @@ class LogisticRegression:
 
         lin_model = np.dot(X_poly_scaled, self.weights) + self.bias
         y_pred = 1 / (1 + np.exp(-lin_model))
-        y_pred_bin = np.where(y_pred > 0.5, 1, 0)
+        y_pred_bin = np.where(y_pred > self.threshold, 1, 0)
         return y_pred_bin
